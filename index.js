cd1b55a69651eb8cec3a06b92377661be313b0c4
Mateo Álvarez <alvarez.fing@gmail.com>
2021-05-31 21:08:34 -0300

    feat(login): add security to login

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const handleErrors = require('./middlewares/handleErrors')
 
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 /*  sirve para permitir a otros dominios consumir la api  */
 app.use(cors())
@@ -19,9 +20,11 @@ app.use(express.json())
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/login', loginRouter)
 
 app.use(notFound)
 app.use(handleErrors)
+
 const PORT = process.env.PORT
 // es async, por lo que cuando termina de hacen la conexion, tira el console.log
 const server = app.listen(PORT, () => {
