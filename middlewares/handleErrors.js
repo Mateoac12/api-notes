38b0ab76f648b227a17052b9aaff915ebbdb3b81
Mateo Álvarez <alvarez.fing@gmail.com>
2021-06-14 23:27:10 -0300

    feat(change register error message): create new error message for register

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,6 +1,6 @@
 
 const ERROR_HANDLERS = {
-  ValidationError: (res, { message }) => res.status(400).send({ err: message }),
+  ValidationError: (res, { message }) => res.status(400).send({ error: message }),
 
   JsonWebTokenError: (res, err) => res.status(401).send({ error: err }),
 
@@ -12,7 +12,7 @@ const ERROR_HANDLERS = {
 
 module.exports = (error, request, response, next) => {
   console.log('pepito error')
-
+  console.log(error.name)
   const handler = ERROR_HANDLERS[error.name] || ERROR_HANDLERS.DefaultError
 
   handler(response, error)
